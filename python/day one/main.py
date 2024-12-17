c80a7d08092f058a73377863dc8fb9a19d25fb64
Mark Solante <marksolante221@gmail.com>
2024-12-17 20:13:44 +0800

    [SOLVED DAY1]

--- python/day one/main.py
+++ python/day one/main.py
@@ -1,8 +1,7 @@
 inp = open("../../input/day1.txt", "r")
 
 
-def main():
-    total = 0
+def splitData():
     left = []
     right = []
     for _ in inp:
@@ -12,13 +11,33 @@ def main():
         left.append(col1)
         right.append(col2)
 
+    return [left, right]
+
+def partOne():
+    total = 0
+    data = splitData()
+    left = data[0]
+    right = data[1]
     left.sort()
     right.sort()
 
     for _ in range(len(left)):
         total = total + (max(left[_], right[_]) - min(left[_], right[_]))
 
-    print(total)
+    return total
+
+def partTwo():
+    total = 0
+    data = splitData()
+    left = data[0]
+    right = data[1]
+
+    for _ in range(len(left)):
+        val = left[_]
+        total = total + ( val * right.count(val))
+
+    return total
 
 
-main()
+
+print(partTwo())
