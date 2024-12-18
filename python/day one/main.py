6df37a220433e12e013bc37352b4b313ecd5345e
DenzeySenpai <marksolante221@gmail.com>
2024-12-18 18:03:43 +0800

    [DAY 2 INCO 2]

--- python/day one/main.py
+++ python/day one/main.py
@@ -6,11 +6,8 @@ def splitData():
     right = []
     for _ in inp:
         row = _.replace("\n","").split("   ")
-        col1 = int(row[0])
-        col2 = int(row[1])
-        left.append(col1)
-        right.append(col2)
-
+        left.append(int(row[0]))
+        right.append(int(row[1]))
     return [left, right]
 
 def partOne():
@@ -20,10 +17,8 @@ def partOne():
     right = data[1]
     left.sort()
     right.sort()
-
     for _ in range(len(left)):
         total = total + (max(left[_], right[_]) - min(left[_], right[_]))
-
     return total
 
 def partTwo():
@@ -31,13 +26,9 @@ def partTwo():
     data = splitData()
     left = data[0]
     right = data[1]
-
     for _ in range(len(left)):
         val = left[_]
         total = total + ( val * right.count(val))
-
     return total
-
-
 
 print(partTwo())
--- python/day two/main.py
+++ python/day two/main.py
@@ -1,33 +1,86 @@
 import numpy as np
+import math as mt
 
-inp = open("../../input/day2.txt", "r")
+inp = open("../../input/day22.txt", "r")
 
-def rec(row, index, val, count):
-    if np.size(row) - 1 == index:
-        if np.size(row) - 1 == count:
-            return val + 1
-        else:
-            return val
-    
-    if row[index] - row[index + 1] == 0:
-        return val
-    if row[index] - row[index + 1] <= 3:
-        return rec(row, index + 1, val, count + 1)
-    else:
-        return rec(row, index + 1, val, count)
-
+def CheckDifference(bigger, smaller):
+    dif = bigger - smaller
+    return  dif > 0 and dif <= 3
 
 def partOne():
     safeCount = 0
     for _ in inp:
+        dampener = 0
         rowR = _.strip().split(" ")
         rowS = np.array([])
         for val in rowR:
             rowS = np.append(rowS,int(val))
-        a = np.sort(rowS)
-        b = np.flip(a)
-        if (a.all() == rowS.all() or b.all() == rowS.all()):
-            safeCount = rec(b, 0, safeCount, 0)
+        isIncreasing = True
+        isValid = True
+        if rowS[0] > rowS[np.size(rowS) - 1]:
+            isIncreasing = False
+        numOfRows = np.size(rowS)
+        for it in range( numOfRows - 1):
+            curr = rowS[it]
+            next = rowS[it + 1]
+            if isIncreasing:
+                curr = rowS[(numOfRows - 1) - it]
+                next = rowS[(numOfRows - 2) - it]
+                if not CheckDifference(curr, next):
+                    if dampener > 0 :
+                        isValid = False
+                        break
+                    else:
+                        dampener = dampener + 1
+        if isValid:
+            print(rowS)
+            safeCount = safeCount + 1
     return safeCount
 
-print(partOne())
+def breakP(value):
+    print(value)
+    # input()
+
+def partTwo():
+    safeCount = 0
+    items = 0
+    for _ in inp:
+        dampener = 0
+        rowR = _.strip().split(" ")
+        rowS = np.array([])
+        items = items + 1
+        for val in rowR:
+            rowS = np.append(rowS,int(val))
+        isIncreasing = True
+        isValid = True
+        numOfRows = np.size(rowS)
+        if rowS[0] > rowS[numOfRows- 1] or (rowS[0] >= rowS[numOfRows- 1] and rowS[numOfRows - 1] < rowS[mt.floor(numOfRows / 2)]):
+            # print("dec ",rowS)
+            isIncreasing = False
+        for va in range(numOfRows - 1):
+            if dampener > 2:
+                isValid = False
+                continue
+            curr = rowS[va]
+            next = rowS[va + 1]
+            if isIncreasing:
+                curr = rowS[numOfRows - 1 - va]
+                next = rowS[numOfRows - 2 - va]
+            if not CheckDifference(curr, next):
+                dampener = dampener + 1
+                val = va + 2
+                if val != np.size(rowS):
+                    next = rowS[val]
+                if isIncreasing:
+                    next = rowS[numOfRows - 3 - va]
+                if (not CheckDifference(curr, next)) and dampener <= 1:
+                    dampener = dampener + 1
+
+        breakP((isValid,dampener,rowS))
+        if isValid:
+            safeCount = safeCount + 1
+    return safeCount, items 
+print(partTwo())
+
+# if unsafe detected, dampener--
+# if dampener is zero and is unsafe
